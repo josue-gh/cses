d2221f0abc1a2000af65e794869a6fd34e179169
josue-gh <josue.mlpro.2014@gmail.com>
2026-02-20 12:32:03 -0500

    modificando permutations y agregando Two sets

--- Permutations.cpp
+++ Permutations.cpp
@@ -1,16 +1,21 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+
 int main(){
-	long long n;
+	
+	long long int n;
 	cin>>n;
-	long long int arr[1000000];
+	vector<long long> arr;
 	long long int pos=0;
+
 	if(n>=4){
 		for(long long int i=1; i<=n; i++){
 			if(i%2==0){
 				cout<<i<<" ";
+
 			}else{
-				arr[pos]=i;
+				arr.push_back(i);
 				pos++;
 			}
 		}
@@ -20,8 +25,10 @@ int main(){
 		}
 	}else if(n==1){
 		cout<<"1"<<endl;
+
 	}else{
 		cout<<"NO SOLUTION"<<endl;
 	}
+
 	return 0;
 }
---
+++ Two_Sets.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long n;
+    cin>>n;
+    long long total=(n*(n+1))/2;
+    int pos=1;
+    int aux=1;
+    int a=0;
+    int arr[1000000];
+    if(total%2==0){
+        cout<<"YES"<<endl;
+        if(n%2==0){
+        	cout<<n/2<<endl;
+            cout<<"1 ";
+            for(int i=2;i<=n;i++){
+                if(pos%2!=0){   
+                    arr[a]=i;
+                    a++;
+                    aux++;
+                    if(aux==3){
+                        aux=1;
+                        pos++;
+                    }
+                }else{
+                    cout<<i<<" ";
+                    aux++;
+                    if(aux==3){
+                        aux=1;
+                        pos++;
+                    }
+                }
+            }
+            cout<<endl;
+            cout<<n/2<<endl;
+            for(int i=0; i<a; i++){
+                cout<<arr[i]<<" ";
+            }
+        }else{
+        	int conjuntos=n/2;
+        	cout<<conjuntos+1<<endl;
+            for(int i=1;i<=n;i++){
+               if(pos%2==0){   
+                    arr[a]=i;
+                    a++;
+                    aux++;
+                    if(aux==3){
+                        aux=1;
+                        pos++;
+                    }
+                }else{
+                    cout<<i<<" ";
+                    aux++;
+                    if(aux==3){
+                        aux=1;
+                        pos++;
+                    }
+                }
+        }
+        cout<<endl;
+        cout<<conjuntos<<endl;
+            for(int i=0; i<a; i++){
+                cout<<arr[i]<<" ";
+            }
+        }
+    }else{
+        cout<<"NO"<<endl;
+    }
+    
+    return 0;
+}
